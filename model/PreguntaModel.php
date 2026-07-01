b7b2d14f33a3bdfc82a2a9ea0ee07b39ea1a4e81
IgnacioPascale00 <nachipascale@gmail.com>
2026-07-01 12:05:03 -0300

    refactorizacion logica de nivel de dificultad de preguntas

--- model/PreguntaModel.php
+++ model/PreguntaModel.php
@@ -9,13 +9,13 @@ class PreguntaModel
         $this->database = $database;
     }
 
-    public function getPreguntaRandom($categoriaId, $dificultad)
+    public function getPreguntaRandom($categoriaId, $nivelJugador)
     {
         $sql = "SELECT id, texto, categoria_id FROM PREGUNTA WHERE categoria_id = ? AND dificultad = ? AND estado = 'aprobada'
         ORDER BY RAND()
         LIMIT 1 ";
 
-        $resultado = $this->database->query($sql, [$categoriaId, $dificultad]);
+        $resultado = $this->database->query($sql, [$categoriaId, $nivelJugador]);
 
         if (!empty($resultado)) {
             return $resultado[0];
@@ -78,5 +78,57 @@ class PreguntaModel
     ";
 
         return $this->database->execute($sql, [$usuarioId, $puntaje]);
+    }
+
+    public function actualizarEstadisticasPregunta($preguntaId, $esCorrecta)
+    {
+        if ($esCorrecta) {
+
+            $sql = "UPDATE PREGUNTA 
+            SET respuestas_correctas = respuestas_correctas + 1
+            WHERE id = ?";
+
+        } else {
+
+            $sql = "UPDATE PREGUNTA
+                    SET respuestas_incorrectas = respuestas_incorrectas + 1
+                    WHERE id = ?";
+
+        }
+        $this->database->execute($sql, [$preguntaId]);
+
+        $this->recalcularDificultad($preguntaId);
+    }
+
+    private function recalcularDificultad($preguntaId)
+    {
+        $sql = "SELECT respuestas_correctas, respuestas_incorrectas
+                FROM PREGUNTA
+                WHERE id = ?";
+
+        $pregunta = $this->database->query($sql, [$preguntaId])[0];
+
+        $correctas = $pregunta["respuestas_correctas"];
+        $incorrectas = $pregunta["respuestas_incorrectas"];
+
+        $total = $correctas + $incorrectas;
+
+        if($total == 0){
+            return;
+        }
+
+        $porcentaje = $correctas / $total;
+
+        if($porcentaje >= 0.70){
+            $dificultad = 0;
+        }elseif($porcentaje >= 0.40){
+            $dificultad = 1;
+        }else{
+            $dificultad = 2;
+        }
+
+        $sql = "UPDATE PREGUNTA SET dificultad=? WHERE id=?";
+
+        $this->database->execute($sql,[$dificultad,$preguntaId]);
     }
 }
